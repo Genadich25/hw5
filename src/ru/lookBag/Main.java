aa5a71add9751825fc5c5c9ba72f774614e6d384
pin20 <pin200048@gmail.com>
2022-01-15 17:32:41 +0300

    Готовые сложные задачки

--- src/ru/lookBag/Main.java
+++ src/ru/lookBag/Main.java
@@ -1,5 +1,5 @@
 package ru.lookBag;
-import javax.xml.crypto.dsig.spec.HMACParameterSpec;
+
 import java.util.Arrays;
 
 public class Main {
@@ -61,6 +61,7 @@ public class Main {
 
         System.out.println(); System.out.println();
 
+    //Сложные задачки
     // 5 задача
 
         int[][] matrix = new int[3][3];
@@ -76,6 +77,83 @@ public class Main {
         System.out.println();
         }
 
+        System.out.println();
+
+    // 6 задача
+
+        int[] justArray = {5, 4, 3, 2, 1};
+        int[] intermediateArray = new int[justArray.length];
+        int j = 0;
+        System.out.println("Решение с использовние доп. массива:");
+
+        System.out.print("  Массив до преобразования: ");
+        System.out.println(Arrays.toString(justArray));
+
+        for (int i = justArray.length - 1; i >= 0; i--) {
+            intermediateArray[j] = justArray[i];
+            j++;
+        }
+
+        for (int i = 0; i < intermediateArray.length; i++) {
+            justArray[i] = intermediateArray[i];
+        }
+
+        System.out.print("  Массив после преобразования: ");
+        System.out.println(Arrays.toString(justArray));
+
+        System.out.println();
+
+    // 7 задача
+
+        int[] justArray2 = {9, 3, 2, 8, 5};
+        int lastIndexJustArray2 = justArray2.length - 1;
+        int middleIndex = (justArray2.length - 1) / 2;
+        int storage;
+
+        System.out.println("Решение в одном массиве:");
+        System.out.println("  Массив до преобразования: " + Arrays.toString(justArray2));
+
+        for (int i = 0; i <= middleIndex; i++) {
+                storage = justArray2[i];
+                justArray2[i] = justArray2[lastIndexJustArray2];
+                justArray2[lastIndexJustArray2] = storage;
+                lastIndexJustArray2--;
+        }
+
+        System.out.println("  Массив после преобразования: " + Arrays.toString(justArray2));
+
+        System.out.println();
+
+    // 8 задача
+
+        System.out.println();
+
+        int[] tenElementsArray = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        int otherIndex = 0;
+
+        for (int i = 0; i < tenElementsArray.length;) {
+            if(tenElementsArray[i] + tenElementsArray[otherIndex] == -2){
+                System.out.println(tenElementsArray[i] + " " + tenElementsArray[otherIndex]);
+                break;
+            }
+            otherIndex++;
+            if (otherIndex == tenElementsArray.length - 1){
+                otherIndex = i;
+                i++;
+            }
+        }
+
+        System.out.println();
+
+    // 9 задача
+
+        for (int i = 0; i < tenElementsArray.length; i++) {
+            for (int k = 0; k <= i; k++) {
+                if(tenElementsArray[i] + tenElementsArray[k] == -2){
+                    System.out.println(tenElementsArray[k] + " " + tenElementsArray[i]);
+                }
+            }
+        }
     }
 
     public static int[] generateRandomArray() {
